0de4520291e0f31bb230b662a7b553d46a83cd47
Philip Levy <philipdlevy@gmail.com>
2022-05-24 12:17:24 -0600

    commit tree a1d72bde258be726b55df923e3ded4286e135d57

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,28 @@
+require 'pry'
+
 class Dog
 
+    # setter method for name
+    def name=(name)
+        @name = name
+    end
+
+    # getter method for name
+    def name 
+        @name
+    end
+
+    # setter method for breed
+    def breed=(breed)
+        @breed = breed
+    end
+
+    # getter method for breed
+    def breed 
+        @breed
+    end
 end
+
+chloe = Dog.new
+chloe.name = "Chloe"
+# binding.pry
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,30 @@
+require "pry"
+
 class Person
 
+    #setter method
+    def name=(name)
+        @name = name
+    end
+
+    #getter method
+    def name
+        @name
+    end
+
+
+    #setter method
+    def job=(job)
+        @job = job
+    end
+
+    #getter method
+    def job
+        @job
+    end
+
 end
+
+name1 = Person.new
+name1.name = "kanye"
+# binding.pry
